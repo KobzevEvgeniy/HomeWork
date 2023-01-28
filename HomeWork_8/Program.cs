81a6213bca11a688d7bad52eda6fc0f85cdda324
Evgeniy <public-judge@yandex.ru>
2023-01-28 12:12:12 +0300

    программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N

---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число N: ");
+int N= int.Parse(Console.ReadLine());
+int K;        //количество четных чисел
+int number;        
+number=1;
+K=0;
+while(number<=N){
+    if (number %2==0){
+        K=K+1; 
+        number++;
+    }
+    else {
+    number++;
+    }   
+}
+Console.Write($"Koличество четных чисел= {K}");
