1fc621a85da9c303cae641347a1475dd32ee9acc
Evgeniy <public-judge@yandex.ru>
2023-01-28 12:02:19 +0300

    ДЗ, программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1= int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2= int.Parse(Console.ReadLine());
+if (number1>number2){
+    Console.Write($"Большее число: {number1}; ");
+    Console.Write($"Наименьшее число: {number2}");
+    return;
+}
+else {
+ Console.Write($"Большее число: {number2}; ");
+ Console.Write($"Наименьшее число: {number1}");
+}
