04bc0e8857aaafa60dcef44e92fd63c9c8878a96
Evgeniy <public-judge@yandex.ru>
2023-01-28 11:59:26 +0300

    программу, которая на вход принимает число, и показывает Числа от -N до N

---
+++ 3/Program.cs
@@ -0,0 +1,9 @@
+﻿//напишите программу, которая на вход принимает число, и показывает Числа от -N до N//
+Console.Clear();
+Console.Write("Введите число: ");
+int number= int.Parse(Console.ReadLine());
+int i=-1*number;
+while(i<=number){
+    Console.Write($"{i}");
+    i++;
+}
