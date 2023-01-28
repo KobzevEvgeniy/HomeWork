7c559ae2700968a0d17895765ef541dbc771f01e
Evgeniy <public-judge@yandex.ru>
2023-01-28 11:58:00 +0300

    Программа сравнивает является число квадратом другого числа

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿//напишите программу, которая на вход принимает два числа, сравнивает явлется ли первое квадратом второго//
+//(число умножемое на себя//
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1*number1==number2){
+Console.WriteLine("Да");
+}
+else{
+Console.WriteLine("Нет");    
+}
